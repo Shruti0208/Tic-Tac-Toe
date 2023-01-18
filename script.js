62415119468ab5cd10137639a36e43dd099a84a1
Shruti0208 <shruti.singh2828@gmail.com>
2023-01-18 17:48:26 +0530

    modified

--- script.js
+++ script.js
@@ -1,31 +1,52 @@
 
 var cur=0;
 var arr=[0,0,0,0,0,0,0,0,0];
+var response=[0,0,0,0,0,0,0,0,0];
 var n=document.querySelectorAll(".cell").length;
 for(let i=0;i<n;i++)
-{   if(arr[i]==0)
-    { document.querySelectorAll(".cell")[i].addEventListener("click",function()
-    {
-        if(cur%2==0)
+{   
+    document.querySelectorAll(".cell")[i].addEventListener("click",function()
+    {  if(arr[i]==0)
+        {if(cur%2==0)
         {   
             document.querySelectorAll(".cell")[i].innerHTML="O";
+           
+            
             arr[i]=1;
             cur++;
+            text="<button onclick='backtrack(cur,i)'> move " + cur + " on block "+ i+" </button>";
+            document.querySelector(".displayMoves").insertAdjacentHTML("afterend",text)
+            response[cur]=i;
         }
         else{
            
             document.querySelectorAll(".cell")[i].innerHTML="X";
+            
+            
             arr[i]=-1;
             cur++;
+            text="<button onclick='backtrack(cur,i)'> move " + cur + " on block "+ i+" </button>";
+            document.querySelector(".displayMoves").insertAdjacentHTML("afterend",text)
+            response[cur]=i;
         }
+    }
         if(cur>=5)
         {
             win();
         }
     });
-}
-}
 
+}
+// function backtrack(index,value)
+// {
+//     for(let i=index;i<9;i++)
+//     {
+//         if(response[i]!==0)
+//         {
+//             document.querySelectorAll(".cell")[response[i]].innerHTML="";
+//         }
+//     }
+// }
 function win()
 {
     if(arr[0]==arr[1] && arr[1]==arr[2] || arr[0]==arr[4]&&arr[4]==arr[8] || arr[0]==arr[3] && arr[3]==arr[6])
